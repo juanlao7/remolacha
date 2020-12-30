19a797db86e0cb93466b4c015a7eed471b9133d8
Juan Lao <nitzing@gmail.com>
2020-12-30 16:41:16 +0100

    Restore body background after exiting from Desktop

--- client/apps/remolacha.Desktop/src/DesktopAppInitializer.ts
+++ client/apps/remolacha.Desktop/src/DesktopAppInitializer.ts
@@ -6,5 +6,7 @@ export default class DesktopAppInitializer {
 
         // TODO: set a background from configuration.
         document.body.style.background = 'url("apps/remolacha.Desktop/backgrounds/4.jpg") center / cover';
+
+        appInstance.events.on('exit', () : any => document.body.style.background = null);
     }
 }
