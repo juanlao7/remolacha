01b233fef3f4d4277d242d3a0bb2727df1fbecf8
Juan Lao <nitzing@gmail.com>
2020-12-30 15:33:39 +0100

    Exposes elements of the module

--- client/remolacha/index.ts
+++ client/remolacha/index.ts
@@ -1,23 +1,27 @@
 import Environment from './Environment';
 import Window from './Window';
+import DataTable from './DataTable';
 import RemolachaIcon from './RemolachaIcon';
 import theme from './theme';
 import * as utils from './utils';
 import AppNotFoundError from './AppNotFoundError';
 import MaxInstancesReachedError from './MaxInstancesReachedError';
 import UndefinedAppInitializerError from './UndefinedAppInitializerError';
+import PermissionDeniedError from './PermissionDeniedError';
 
 require('./assets/sass/index.sass');
 
 export {
     Environment,
     Window,
+    DataTable,
     RemolachaIcon,
     theme,
     utils,
     AppNotFoundError,
     MaxInstancesReachedError,
     UndefinedAppInitializerError,
+    PermissionDeniedError
 };
 
 document.addEventListener('DOMContentLoaded', async () => {
